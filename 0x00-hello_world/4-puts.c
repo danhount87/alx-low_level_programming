e7ff0c0c2eac5c304f659b9fa061cc9736ea313a
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-08 04:44:17 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Our program want to show a message
+ * without us using the printf function
+ */
+int main(void)
+        {
+	put ("\"Programming is like building a multilingual puzzle");
+	return (0);
+	}
