39ad9a9787605c1c7c20c1a60dc11109bc084575
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-08 05:03:58 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Our program want to show a message
- * without us using the printf function
+ * main - main block
+ * Return: 0
  */
 int main(void)
-        {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-	}
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
