86dacec682cebbd44500bceb6ae66d27fd8340a6
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-13 13:40:17 -0800

    multiplication-table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
+ */
+
+void print_times_table(int n)
+{
+	int nk, m, p;
+
+	if (nk >= 0 && nk <= 15)
+	{
+		for (nk = 0; nk <= n; nk++)
+		{
+			_putchar('0');
+
+			for (m = 0; m <= n; m++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				p = nk * m;
+
+				if (p <= 99)
+					_putchar(' ');
+				if (p <= 9)
+					_putchar(' ');
+
+				if (p >= 100)
+				{
+					_putchar((p / 100) + '0');
+					_putchar(((p / 10)) % 10 + '0');
+				}
+				else if (p <= 99 && p >= 10)
+				{
+					_putchar((p / 10) + '0');
+				}
+				_putchar((p % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
