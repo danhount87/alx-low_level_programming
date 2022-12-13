29225ac30ed244f56faa90e4f020ab1d2d9d45ad
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-13 06:47:21 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,13 @@
 /**
  * _islower - Entry point
  * Our function checks for lowercase character.
- * Return: 0 if character is lowercase, 1 otherwise
+ * Return: 1 or 0
  */
 
 int _islower(int c)
 {
-	if (c >= 'z' && c <= 'a')
-		return (0);
-	else
+	if (c >= 'a' && c <= 'z')
 		return (1);
+	else
+		return (0);
 }
