e06d75e9e3a6ffd3e945884e5a512dd4b2327fce
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-13 08:19:24 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,12 +4,10 @@
  * _isalpha - Entry point
  * Our function checks for alphabetic character.
  * @c : this character will be checked
- * return: 1 or 0
+ * return: 0 or 1
  */
+
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+	return ((c >= '97' && c <= '122') || (c >= '65' && c <= '90'));
 }
