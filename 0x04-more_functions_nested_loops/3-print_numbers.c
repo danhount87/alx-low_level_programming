880787d3be74ba6969b8d167eec3771436750599
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-15 06:17:24 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,15 @@
 
 /**
  * print_numbers - prints the numbers
+ * Return: 0
  */
 
 void print_numbers(void)
 {
-	char j = 0;
+	int j;
 
-	while (j <= 9)
-	{
-		_putchar('0' + j);
-		j++;
-	}
+	for (j = 0; j <= 9; j++)
+		_putchar((j % 10) + '0');
+
 	_putchar('\n');
 }
