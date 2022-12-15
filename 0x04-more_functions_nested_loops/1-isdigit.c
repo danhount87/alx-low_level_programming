7ebfa14b04be79e543d706ebfa379006ddd52e6e
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-15 03:04:47 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: variable to be used in the function
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
