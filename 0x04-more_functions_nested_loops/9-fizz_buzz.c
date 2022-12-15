2da1b2c9c5f188343cd5f24a5b9334df4632a00b
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-15 09:19:58 -0800

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - “Fizz-Buzz test” is an interview question
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int j = 1;
+
+	while (j <= 100)
+	{
+	if (j % 3 == 0 && j % 5 == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else if (j % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (j % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%i", j);
+	}
+
+if (j != 100)
+{
+printf(' ');
+}
+
+j++;
+}
+putchar('\n');
+return (0);
+
+}
