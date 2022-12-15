cdcc2517e10aa31a492c10817230ad15b8984b4d
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-15 08:01:21 -0800

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 except 2 & 4
+ */
+
+void print_most_numbers(void)
+{
+	char j = 0;
+
+	while (j <= 9)
+	{
+		if (j != 2 && j != 4)
+		{
+			_putchar('0' + j);
+		}
+
+		j++;
+	}
+	_putchar('\n');
+
+}
+
