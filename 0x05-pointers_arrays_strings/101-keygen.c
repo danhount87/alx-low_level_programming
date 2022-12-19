152ebc3756ff12ba4b25c5b82beb7b273feb3ef3
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-19 12:50:53 -0800

    hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid
+ * passwords for the program 101-crackme
+ * Return: 0
+ */
+
+int main(void)
+{
+	int j, sm, k;
+	int ps[100];
+
+	sm = 0;
+
+	srand(time(NULL));
+
+	for (j = 0; j < 100; j++)
+	{
+		ps[i] = rand() % 78;
+		sm += (ps[j] + '0');
+		putchar(ps[j] + '0');
+		if ((2772 - sm) - '0' < 78)
+		{
+			k = 2772 - sm - '0';
+			sm += k;
+			putchar(k + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
