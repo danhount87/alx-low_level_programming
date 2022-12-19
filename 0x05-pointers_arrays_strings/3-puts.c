5027df1bd92bd418c2b2501741d189efff82c588
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-19 08:10:30 -0800

    not fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: the pointer to the string to print
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
