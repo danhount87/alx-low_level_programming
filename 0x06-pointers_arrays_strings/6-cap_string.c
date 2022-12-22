2204b08e55b766af5816349c3c9e420ce580f03a
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2022-12-22 06:57:11 -0800

    Expect the best

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: pointer
+ * Return: pointer to the change string
+ */
+
+char *cap_string(char *str)
+{
+	int dx = 0;
+
+	while (str[dx])
+	{
+		while (!(str[dx] >= 'a' && str[dx] <= 'z'))
+			dx++;
+
+		if (str[dx - 1] == ' ' || str[dx - 1] == '\t' || str[dx - 1] == '\n' ||
+		    str[dx - 1] == ',' || str[dx - 1] == ';' || str[dx - 1] == '.' ||
+		    str[dx - 1] == '!' || str[dx - 1] == '?' || str[dx - 1] == '"' ||
+		    str[dx - 1] == '(' || str[dx - 1] == ')' || str[dx - 1] == '{' ||
+		    str[dx - 1] == '}' || index == 0)
+			str[dx] -= 32;
+
+		dx++;
+	}
+
+	return (str);
+}
