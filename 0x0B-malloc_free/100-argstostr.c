639f47fec79dcb2e73afc2fc8fa3f96798d02022
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-01-09 09:35:24 -0800

    the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the arguments
+ * of your program.
+ * @ac: number of arguments
+ * @av: array of pointer
+ * Return: the arguments
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int cnt = 0, x = 0, y = 0, z = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (x < ac)
+	{
+		b = 0;
+		while (av[x][y] != '\0')
+		{
+			cnt++;
+			y++;
+		}
+		x++;
+	}
+	cnt = cnt + ac + 1;
+	str = malloc(sizeof(char) * cnt);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			str[z] = av[x][y];
+			z++;
+		}
+		str[z] = '\n';
+		z++;
+	}
+	return (str);
+}
