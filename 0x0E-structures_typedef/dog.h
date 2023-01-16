5258cc33339d668588507b0180ba2513e3aeae09
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-01-16 03:54:58 -0800

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
-
+/* @zinzinpolice */
 /**
  * struct dog - a dog's basic info
  * @name: 1st member
@@ -21,7 +21,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
