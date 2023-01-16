907e2b4aec7179f818dcf2cdf0bd9cf56c75b453
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-01-16 08:45:06 -0800

    SUM

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,6 +1,6 @@
 #ifndef PI_H
 #define PI_H
-
+/* @zinzinpolice */
 #define PI 3.14159265359
 
 #endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-
+/* @zinzinpolice */
 #define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
