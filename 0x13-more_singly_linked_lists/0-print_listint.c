bbe24f74b101476992d686886e12b119d1207074
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-01-30 02:23:06 -0800

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/* @zinzinpolice */
+/**
+ * print_listint - function that prints all
+ * the elements of a linked list
+ * @h: linked list
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t publ = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		publ++;
+		h = h->next;
+	}
+	return (publ);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+our project in this repository are based to train us on more singly linked list
