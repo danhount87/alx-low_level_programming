f3df9748022bf320f8a7dd5e02f91f562835e7ff
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-02-02 08:58:31 -0800

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/* @zinzinpolice */
+/**
+ * get_endianness - function that checks the endianness.
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void);
+{
+	unsigned int j = 1;
+	char *c = (char *) &j;
+	return (*c);
+}
