9ec8ccfe8bbaa72348c6260b3062a18d71223966
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-02-02 09:14:27 -0800

    Endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,7 +4,7 @@
  * get_endianness - function that checks the endianness.
  * Return: 0 if big endian, 1 if little endian
  */
-int get_endianness(void);
+int get_endianness(void)
 {
 	unsigned int j = 1;
 	char *c = (char *) &j;
