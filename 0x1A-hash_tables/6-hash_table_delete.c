bc1ec9452dad81b8d9344b0d77934aa10c2dc20f
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-04-06 10:46:39 -0700

    >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /*@zinzinpolice*/
 /**
- * hash_table_delete - 
+ * hash_table_delete - function that deletes a hash table.
  * @ht: hash table to delete
  *
  * Return: void
