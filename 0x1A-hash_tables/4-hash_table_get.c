2b2144b5d9ea7dcb8f2321571113a8ace393fb84
Daniel F. HOUNTONDJI <zinzinpolice@gmail.com>
2023-04-06 10:23:27 -0700

    >>> ht[betty]

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/*@zinzinpolice*/
+/**
+ * hash_table_get - function that retrieves a value associated
+ * with a key.
+ * @ht: table to retrieve value from
+ * @key: key to find value
+ * Return: value associated with key, or NULL if key cannot be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *tmp;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
